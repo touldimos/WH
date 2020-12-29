00fe40869fe60da536d6968864108d29316d546f
touldimos <64074305+touldimos@users.noreply.github.com>
2020-12-29 12:47:19 +0200

    Add files via upload

--- wh_char.py
+++ wh_char.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from ipywidgets import interact, interactive, fixed, interact_manual
 
 #Properties of the fluid
 K = 0.001                           #Pipe roughness [mm]
@@ -42,18 +43,6 @@ def fcw(n, D, v, K):
 
 f = fcw(n, Din, v, K)
 
-# f0 = (1/(2*np.log10(K/(3.72*Din*1000))))**2
-
-# def f(f):
-#     Re = v*Din/1000/n
-#     F = (1/(2*np.log10((K/(3.72*Din*1000)) + (2.51/(Re*np.sqrt(f))))))**2 - f
-#     return F
-# f = fsolve(f, f0)
-
-# Re = 4 * np.abs(Q) / (np.pi * Din * n)
-# rhs = -1.793 * np.log10((K / (3.7 * Din))**1.114 + 6.925/Re)
-# f = (1 / rhs)**2
-
 hf = f*l*v**2/(Din*2*g)
 hl = hfl/100*hf
 dh = hf + hl
@@ -79,22 +68,21 @@ for i in range(nodes):
     hhigh[i] = h[i]
     pipez[i] = elevup - delel * i
     head[i] = h[i] - pipez[i]
-   
+
 Qic = np.ones(nodes) * Q
 Vic = np.ones(nodes) * v
 Hic = h
 
-def chars(nodes, Q, D, n, K, H, ca, dt, hzero, V0, t, tclose):
+def chars(nodes, Q, D, n, K, H, ca, dt, hzero, V0, t, tclose, A):
     def fcw(n, D, v, K):
         midf = []
         Re = v * D / n
-        f = (1/(2*np.log10(K/(3.72*D*1000))))**2
+        f = (1/(2*np.log10(K/(3.72 * D * 1000))))**2
         midf.append(f)
         for i in range(1, 10):
-            midf.append((1/(2*np.log10((K/(3.72*D*1000)) + (2.51/(Re*np.sqrt(midf[i - 1]))))))**2)
+            midf.append((1 / (2 * np.log10((K / (3.72 * D * 1000)) + (2.51/(Re * np.sqrt(midf[i - 1]))))))**2)
         f = midf[-1]
         return f
-
     cm_ = np.zeros(nodes)
     cn_ = np.zeros(nodes)
     Hnew = np.zeros(nodes)
@@ -102,7 +90,8 @@ def chars(nodes, Q, D, n, K, H, ca, dt, hzero, V0, t, tclose):
     Vnew = np.zeros(nodes)
     for i in range(1, nodes):
         if Q[i - 1] != 0:
-            f = fcw(n, D, v, K)
+            vi = Q[i - 1] / A
+            f = fcw(n, D, np.abs(vi), K)
         else:
             f = 0
         cfa = f * dt / (np.pi * D ** 3/2)
@@ -110,7 +99,8 @@ def chars(nodes, Q, D, n, K, H, ca, dt, hzero, V0, t, tclose):
         cm_[i] = cm
     for i in range(0, nodes - 1):
         if Q[i + 1] != 0:
-            f = fcw(n, D, v, K)
+            vi = Q[i + 1] / A
+            f = fcw(n, D, np.abs(vi), K)
         else:
             f = 0
         cfb = f * dt / (np.pi * D ** 3/2)
@@ -138,23 +128,15 @@ H.append(Hic)
 Q.append(Qic)
 V.append(Vic)
 
-H.append(chars(nodes, Qic, Din, n, K, Hic, ca, dt, hzero, v, dt, tclose)[0])
-Q.append(chars(nodes, Qic, Din, n, K, Hic, ca, dt, hzero, v, dt, tclose)[1])
-V.append(chars(nodes, Qic, Din, n, K, Hic, ca, dt, hzero, v, dt, tclose)[2])
+H.append(chars(nodes, Qic, Din, n, K, Hic, ca, dt, hzero, v, dt, tclose, A)[0])
+Q.append(chars(nodes, Qic, Din, n, K, Hic, ca, dt, hzero, v, dt, tclose, A)[1])
+V.append(chars(nodes, Qic, Din, n, K, Hic, ca, dt, hzero, v, dt, tclose, A)[2])
 
 for i in range(1, len(step)):
-    H.append(chars(nodes, Q[i], Din, n, K, H[i], ca, dt, hzero, v, step[i], tclose)[0])
-    Q.append(chars(nodes, Q[i], Din, n, K, H[i], ca, dt, hzero, v, step[i], tclose)[1])
-    V.append(chars(nodes, Q[i], Din, n, K, H[i], ca, dt, hzero, v, step[i], tclose)[2])
+    H.append(chars(nodes, Q[i], Din, n, K, H[i], ca, dt, hzero, v, step[i], tclose, A)[0])
+    Q.append(chars(nodes, Q[i], Din, n, K, H[i], ca, dt, hzero, v, step[i], tclose, A)[1])
+    V.append(chars(nodes, Q[i], Din, n, K, H[i], ca, dt, hzero, v, step[i], tclose, A)[2])
 
 Head = pd.DataFrame(H)
 Discharge = pd.DataFrame(Q)
 Velocity = pd.DataFrame(V)
-
-plt.plot(Head[10], 'g', zorder = 10)
-plt.hlines(0, -10, len(step), 'black', zorder = 9)
-plt.vlines(0, -abs(Head[10].min()), Head[10].max(), 'black', zorder = 8)
-plt.legend(['Transient Head'])
-plt.title('Node {}'.format(10))
-plt.xlabel('Time [sec]')
-plt.ylabel('Head [m]');
